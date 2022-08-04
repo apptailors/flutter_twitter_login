124c36d4f4ff39b6d42bf8ce87f0a2d059c7a69e
kamilszpila <38101560+kamilszpila@users.noreply.github.com>
2022-08-04 13:21:45 +0200

    Feature/migrate library to android v2 embedding (#2)
    * Update .gitignore files
    
    * Migrate example app to Android v2 embedding
    
    * Update app and library gradle config
    
    * Migrate plugin to v2 embedding
    
    * Delete splash screen from manifest

--- android/src/main/java/com/roughike/fluttertwitterlogin/fluttertwitterlogin/TwitterLoginPlugin.java
+++ android/src/main/java/com/roughike/fluttertwitterlogin/fluttertwitterlogin/TwitterLoginPlugin.java
@@ -1,8 +1,11 @@
 package com.roughike.fluttertwitterlogin.fluttertwitterlogin;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.webkit.CookieManager;
-import android.webkit.CookieSyncManager;
+import android.webkit.ValueCallback;
+
+import androidx.annotation.NonNull;
 
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Twitter;
@@ -15,37 +18,42 @@ import com.twitter.sdk.android.core.identity.TwitterAuthClient;
 
 import java.util.HashMap;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-public class TwitterLoginPlugin extends Callback<TwitterSession> implements MethodCallHandler, PluginRegistry.ActivityResultListener {
+public class TwitterLoginPlugin extends Callback<TwitterSession> implements MethodCallHandler, PluginRegistry.ActivityResultListener, FlutterPlugin, ActivityAware {
     private static final String CHANNEL_NAME = "com.roughike/flutter_twitter_login";
     private static final String METHOD_GET_CURRENT_SESSION = "getCurrentSession";
     private static final String METHOD_AUTHORIZE = "authorize";
     private static final String METHOD_LOG_OUT = "logOut";
 
-    private final Registrar registrar;
+    private ActivityPluginBinding activityPluginBinding;
+    private Activity pluginActivity;
+    private MethodChannel channel;
 
     private TwitterAuthClient authClientInstance;
     private Result pendingResult;
 
-    public static void registerWith(Registrar registrar) {
-        final TwitterLoginPlugin plugin = new TwitterLoginPlugin(registrar);
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_NAME);
-        channel.setMethodCallHandler(plugin);
-    }
-
-    private TwitterLoginPlugin(Registrar registrar) {
-        this.registrar = registrar;
-        registrar.addActivityResultListener(this);
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        final MethodChannel channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL_NAME);
+        this.channel = channel;
+        channel.setMethodCallHandler(this);
     }
 
     @Override
-    public void onMethodCall(MethodCall call, Result result) {
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, @NonNull Result result) {
         switch (call.method) {
             case METHOD_GET_CURRENT_SESSION:
                 getCurrentSession(result, call);
@@ -85,7 +93,7 @@ public class TwitterLoginPlugin extends Callback<TwitterSession> implements Meth
 
     private void authorize(Result result, MethodCall call) {
         setPendingResult("authorize", result);
-        initializeAuthClient(call).authorize(registrar.activity(), this);
+        initializeAuthClient(call).authorize(pluginActivity, this);
     }
 
     private TwitterAuthClient initializeAuthClient(MethodCall call) {
@@ -101,7 +109,7 @@ public class TwitterLoginPlugin extends Callback<TwitterSession> implements Meth
 
     private TwitterAuthClient configureClient(String consumerKey, String consumerSecret) {
         TwitterAuthConfig authConfig = new TwitterAuthConfig(consumerKey, consumerSecret);
-        TwitterConfig config = new TwitterConfig.Builder(registrar.context())
+        TwitterConfig config = new TwitterConfig.Builder(pluginActivity)
                 .twitterAuthConfig(authConfig)
                 .build();
         Twitter.initialize(config);
@@ -110,9 +118,15 @@ public class TwitterLoginPlugin extends Callback<TwitterSession> implements Meth
     }
 
     private void logOut(Result result, MethodCall call) {
-        CookieSyncManager.createInstance(registrar.context());
         CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.removeSessionCookie();
+        cookieManager.removeSessionCookies(
+                new ValueCallback<Boolean>() {
+                    @Override
+                    public void onReceiveValue(Boolean aBoolean) {
+                        // Do nothing
+                    }
+                }
+        );
 
         initializeAuthClient(call);
         TwitterCore.getInstance().getSessionManager().clearActiveSession();
@@ -166,5 +180,39 @@ public class TwitterLoginPlugin extends Callback<TwitterSession> implements Meth
         }
 
         return false;
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        handleAttachToActivity(binding);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        handleDetachFromActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        handleDetachFromActivity();
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        handleAttachToActivity(binding);
+    }
+
+    private void handleAttachToActivity(@NonNull ActivityPluginBinding binding) {
+        activityPluginBinding = binding;
+        pluginActivity = binding.getActivity();
+        activityPluginBinding.addActivityResultListener(this);
+    }
+
+    private void handleDetachFromActivity() {
+        if (activityPluginBinding != null) {
+            activityPluginBinding.removeActivityResultListener(this);
+        }
+        activityPluginBinding = null;
+        pluginActivity = null;
     }
 }
--- example/android/app/src/main/kotlin/com/roughike/fluttertwitterlogin/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.roughike.fluttertwitterlogin.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
